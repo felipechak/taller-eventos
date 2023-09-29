b80c5eff94c9b86700ca8adbcff6736f130bd544
felipechak <felipepacin7@gmail.com>
2023-09-29 18:21:10 -0300

    Fixed false div alert issue

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementsByTagName("div")[0].addEventListener("click",()=>{
-        alert("Hola! soy el div")
+    document.getElementsByTagName("button")[0].addEventListener("click",(ev)=>{
+        alert("Hola!")
+    })
+    document.getElementsByTagName("div")[0].addEventListener("click",(ev)=>{
+        if (ev.target.localName !== "button") {
+            alert("Hola! soy el div")
+        }
+        
     })
 })
 
